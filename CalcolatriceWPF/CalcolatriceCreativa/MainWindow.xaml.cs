8812d7cc7827061e8d18ae9f6cf26520d13e964b
LianTorrico <157365006+LianTorrico@users.noreply.github.com>
2025-01-25 18:00:08 +0100

    v1.0
    Working program, the percentual function still isnt available.

--- CalcolatriceWPF/CalcolatriceCreativa/MainWindow.xaml.cs
+++ CalcolatriceWPF/CalcolatriceCreativa/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -84,6 +85,84 @@ namespace CalcolatriceCreativa
                 equation = equation + "+";
                 TXB_equation.Text = equation;
             }
+        }
+
+        private void BTN_MINUS_Click(object sender, RoutedEventArgs e)
+        {
+            char equationhelper = TXB_equation.Text[TXB_equation.Text.Length - 1];
+            if (equationhelper != '+' && equationhelper != '-' && equationhelper != '*' && equationhelper != '.')
+            {
+                equation = equation + "-";
+                TXB_equation.Text = equation;
+            }
+        }
+
+        private void BTN_MULTI_Click(object sender, RoutedEventArgs e)
+        {
+            char equationhelper = TXB_equation.Text[TXB_equation.Text.Length - 1];
+            if (equationhelper != '+' && equationhelper != '-' && equationhelper != '*' && equationhelper != '.')
+            {
+                equation = equation + "*";
+                TXB_equation.Text = equation;
+            }
+        }
+
+        private void BTN_DOT_Click(object sender, RoutedEventArgs e)
+        {
+            char equationhelper = TXB_equation.Text[TXB_equation.Text.Length - 1];
+            if (equationhelper != '+' && equationhelper != '-' && equationhelper != '*' && equationhelper != '.')
+            {
+                equation = equation + ".";
+                TXB_equation.Text = equation;
+            }
+        }
+
+        private void BTN_NEGATE_Click(object sender, RoutedEventArgs e)
+        {
+            var parts = equation.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length > 0) {
+                string lastNumber = parts[parts.Length - 1];
+                string negatedNumber = "-" + lastNumber;
+                string newEquation = equation.Substring(0, equation.LastIndexOf(lastNumber)) + negatedNumber;
+                equation = newEquation;
+            }
+            TXB_equation.Text = equation;
+        }
+
+        private void BTN_CANC_Click(object sender, RoutedEventArgs e)
+        {
+            string newEquation = "";
+            for (int i = 0; i < equation.Length-1; i++) {
+                newEquation += equation[i];
+            }
+            equation = newEquation;
+            TXB_equation.Text = equation;
+        }
+
+        private void BTN_DELETE_Click(object sender, RoutedEventArgs e)
+        {
+            equation = "";
+            TXB_equation.Text = equation;
+        }
+
+        private void BTN_DELETEALL_Click(object sender, RoutedEventArgs e)
+        {
+            equation = "";
+            TXB_equation.Text = equation;
+        }
+
+        private void BTN_PERCENT_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WIP");
+        }
+
+        private void BTN_EQU_Click(object sender, RoutedEventArgs e)
+        {
+
+            var result = new DataTable().Compute(equation, null);
+            double resolver = Convert.ToDouble(result);
+            resolver += 0;
+            TXB_equation.Text = Convert.ToString(resolver);
         }
     }
 }
