e6d16275bd72e92c62d0cb50a1e97f08eb78d3cc
Tommaso Mambelli <TOMMASO.MAMBELLI@local.ittcesena.it>
2025-03-05 10:55:52 +0100

    Initial commit of MemoryCards Game

---
+++ MemoryGame/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace memory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Start();
+
+        }
+        private static void Shuffle<T>(T[] array)
+        {
+            Random random = new Random();
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+
+            for (int i = 0; i < array.Length - 1; ++i)
+            {
+                int r = random.Next(i, array.Length);
+                (array[r], array[i]) = (array[i], array[r]);
+            }
+        }
+    }
+}
